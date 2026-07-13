0f0431d0f125b1d519ad5e167b3fb9841c63b2f2
olumild <olumild998@gmail.com>
2026-07-13 14:20:15 +0100

    hello world

--- helloworld/src/helloworld/App.java
+++ helloworld/src/helloworld/App.java
@@ -2,11 +2,11 @@ package helloworld;
 
 @SuppressWarnings("java:S106")
 
-
-
 public class App {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("you are welcome");
+        System.out.println("start coding in java");
+
     }
 }
